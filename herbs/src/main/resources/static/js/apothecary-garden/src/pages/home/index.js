d7aef90e25d7582b8e7ab2678e371f70b673cc14
Nuhamin-Tesfaye <nuhamintesfaye11@gmail.com>
2023-02-05 14:26:28 -0500

    adding home page

---
+++ herbs/src/main/resources/static/js/apothecary-garden/src/pages/home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./style.module.scss";
+
+
+const HomePage = () => {
+
+    return (
+        <div className={style.home}>
+            <h1>This is home page</h1>
+         </div> 
+        ); 
+}
+
+export default HomePage;
