4d3bdf65df0a2566420e9be64682f2d2c1254309
campbene <campbell20nate@gmail.com>
2023-02-11 18:23:51 -0500

    added first round of css for disclaimer and adjusted css on illness_search to help with readability for MVP

--- herbs/src/main/resources/static/js/apothecary-garden/src/pages/home/index.js
+++ herbs/src/main/resources/static/js/apothecary-garden/src/pages/home/index.js
@@ -8,7 +8,7 @@ import style from "./style.module.scss";
 const HomePage = () => {
     const [open, setOpen] = React.useState(true)
     return(
-        <div>
+        <div className={style.homeContent}>
             <h1>This is the Home Page</h1>
             <button className={style.disclaimer_button} onClick={() => setOpen(true)}>Disclaimer</button>
             <Disclaimer setOpen={setOpen} open={open} />
