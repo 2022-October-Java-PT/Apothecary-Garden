4f3d22b2890ecd717aa2644f5b346120ff401d9d
WolfgangA1 <washcraft10@gmail.com>
2023-02-19 13:04:16 -0500

    made a login page and a common file in components

---
+++ herbs/src/main/resources/static/js/apothecary-garden/src/pages/login/index.js
@@ -0,0 +1,67 @@
+import React, {useEffect, useState} from 'react';
+import {getUsername, setUsernameSession} from '../../components/common'
+
+import Axios from 'axios';
+import style from './style.module.scss';
+
+const Login = (props) => {
+    
+    const [userName, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const fetchData = async () => {
+            const result = await Axios(`http://localhost:8080/api/user/${userName}`);
+            console.log(result.data);
+            setUsernameSession(result.data.setUsername);
+            props.history.push('/portal');
+        }
+
+        fetchData();
+    }
+
+    console.log("GET USER NAME", getUsername());
+
+    return (
+        <div className={style.loginPage}>
+            
+            <section className={style.loginTwo}>
+                <h2 className={style.loginH2}>Login To Your Account</h2>
+                <form className={style.loginForm} onSubmit={handleSubmit}>
+                    <input className={style.loginText} 
+                        type='text' 
+                        placeholder='UserName'
+                        value={userName}
+                        onChange={(event) => setUsername(event.target.value)}
+                        onFocus={(e) => e.target.placeholder = ""}
+                        onBlur={(e) => e.target.placeholder = "Username"}
+                    />
+                    <input className={style.loginText}
+                        type='text'
+                        placeholder='Password'
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                        onFocus={(e) => e.target.placeholder = ""}
+                        onBlur={(e) => e.target.placeholder = "Password"}
+                    />
+                    <button className={style.loginSubmit} onClick={() => (window.location.href='/portal')} type="submit">Login With Username</button>
+                </form>
+
+                <div className={style.loginLine}>Or</div>
+                <button className={style.loginFace} type="submit">Login With Facebook</button>
+                <button className={style.loginGoogle} type="submit">Login With Google</button>
+            </section>
+
+            <section className={style.loginOne}>
+                <h1 className={style.loginH1}>Welcome Back {getUsername()} We're Glad You're Here.</h1>
+            </section>
+
+            
+        </div>
+    )
+}
+
+export default Login;
